eb2dab999516b5b403c8679983dd5797011a657e
Sardor ibn Farhod <92437771+SardorDev12@users.noreply.github.com>
2023-02-22 14:32:34 +0500

    Fix repo

--- RegisterMyBooks_00012453/RegisterMyBooks_00012453/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RegisterMyBooks_00012453.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
